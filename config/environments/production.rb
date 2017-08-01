3ad723ee9ed671956edfe33b59c50c1651578ebe
karlxavier <kxdc1025@yahoo.com>
2017-08-01 19:17:16 +0400

    heroku deploy 18

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,11 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
-  config.assets.compile = true
+  
   # config.assets.initialize_on_precompile = false
   # config.assets.enabled = true
 
