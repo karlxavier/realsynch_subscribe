3d6d2ff2835cc7589226bd73398236421b8d7c0a
karlxavier <kxdc1025@yahoo.com>
2017-08-01 18:37:53 +0400

    heroku deploy 12

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 
   # config.assets.initialize_on_precompile = false
-  # config.serve_static_assets = true
+  config.serve_static_assets = true
   # config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
